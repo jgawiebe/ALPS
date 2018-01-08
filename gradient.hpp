a52be990cba4ba823ead06bdadf26e806ea1d73a
jgawiebe <31968531+jgawiebe@users.noreply.github.com>
2018-01-08 15:31:51 -0500

    Cleaned up declarations, removed integers

--- gradient.hpp
+++ gradient.hpp
@@ -5,12 +5,6 @@ Rev1: Nov 2017
 */
 #include <iostream>
 #include <armadillo>
-#include "conversion.hpp"
-
-//put these in generic file
-//#define Q 2048 //this is 2^11 (fractional bits)
-//typedef std::vector<int> ivector; //simplifies writing int vector
-
 
 using namespace std;
 using namespace arma;
@@ -23,16 +17,13 @@ void gradient (T &x_deriv, T &y_deriv, T &I){
   int variance = 1;
   int denominator = 2;
 
-  vec numerator;
-  vec derivative;
-  vec temp;
-  ivector itemp;
-  ivector ideriv;
+  vec numerator, derivative, temp;
+  //ivector itemp, ideriv;
 
   //function assumes variance is constant, otherwise use:
-  //variance = sigma*sigma;
-  //denominator = 2 * variance;
-  //derivative = derivative % (temp / variance);
+    //variance = sigma*sigma;
+    //denominator = 2 * variance;
+    //derivative = derivative % (temp / variance);
 
   //gaussDeriv
   //http://arma.sourceforge.net/docs.html#linspace
@@ -43,13 +34,6 @@ void gradient (T &x_deriv, T &y_deriv, T &I){
   derivative = exp(-numerator/denominator) / pow((datum::pi * denominator), 0.5);
 
   derivative = derivative % temp; //can't perform % with std vectors
-  //ideriv = convertv(derivative); //convert from vec to v<int>
-
-
-  //for testing:
-  // for(int i = 0; i < derivative.size(); i++) {
-  //   cout << derivative[i] << endl;
-  // }
 
   x_deriv = conv2( I, derivative, "same" ) ;
   y_deriv = conv2( I, derivative.t(), "same" ) ;
