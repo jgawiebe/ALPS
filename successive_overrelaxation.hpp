c9d066d18b8b0754cae3075a211c19cf79c48839
jgawiebe <31968531+jgawiebe@users.noreply.github.com>
2018-01-10 07:22:24 -0500

    Code complete

--- successive_overrelaxation.hpp
+++ successive_overrelaxation.hpp
@@ -2,6 +2,7 @@
 energy_calc.hpp
 Jacob Wiebe & James Dolman
 Rev1: Nov 2017
+Rev2: Jan 2018
 */
 
 #include <iostream>
@@ -11,36 +12,59 @@ using namespace std;
 using namespace arma;
 
 //M: sor
+//the solution 'x' is the vector 'duv'
+//failure flag can also be used outside this function
 vec successive_overrelaxation (int& failure, mat A, vec x, vec duv, vec b, double omega, int inner_iter, double tolerance){
-
+	//temp variables for matrix splitting
+	mat M, N;
+	
+	failure = 0;
+	
 	double norml = norm(b);
 	if (norml == 0) {
 		norml = 1;
 	}
 
-	vec r = b-A*x;
+	vec r = b - (A*x);
 	error = norm(r)/norml;
-	if (error<tolerance) {
+	if (error < tolerance) {
 		return; //report error
 	}
+	
+	//M, N are outputs; b is an inout
+	split(M, N, b, A, omega);
+	
+	//continue to perform approximations until max iterations or accuracy is below the tolerance level
+	for (int i = 0; i < inner_iter; i++) {
+		vec x_initial = x;
+		mat approx = (N * x) + b;
+		
+		x = solve(M, approx);
+		error = norm(x - x_initial) / norm(x);
+		if (error <= tolerance) {
+			break; //approximation is within tolerance
+		}
+	}
+	
+	//what does this effect? b & r aren't used anywhere after this
+	b =/ omega;
+	r = b - (A * x);
+	
+	if (error > tolerance) {
+		failure = 1; //convergence not found
+	}
 
-
-
-	//non-matrices: gamma, ht, wt
-	//vectors: vals, b, cols, ind, pdfaltsumu/v
-	//sparse: A
-
-  return duv;
+  return x; //solution vector (duv)
 }
 
 //complete
-void split (mat& M, mat& N, vec& b, mat A, double b, double omega) {
+void split (mat& M, mat& N, vec& b, mat A, double omega) {
 	//omega is the relaxation scalar
 	double height = A.n_rows;
 	double width = A.n_cols;
 	mat diagA = diag(diag( A ));
 	
-	b = omega * b;
-	M =  omega * (trimatl( A, -1) + diagA);
+	b =* omega;
+	M = omega * (trimatl( A, -1) + diagA);
 	N = -omega * (trimatu(A, 1) + ((1-omega) * diagA));
 }
