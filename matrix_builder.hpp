14273a9bd38d218cb3f8c1761c387f78ee330543
s27280 <s27280@lab4126-10.ece.rmc.ca>
2018-01-10 17:36:34 -0500

    Wednesday work up to 5 :35, needs a few tests by Wiebe

--- matrix_builder.hpp
+++ matrix_builder.hpp
@@ -10,12 +10,58 @@ Rev1: Nov 2017
 using namespace std;
 using namespace arma;
 
-//M: constructMatrix
-void build_matrix (){
+mat u ;
+mat E_smooth;
+int height = 0;
+int width = 0;
 
-	//non-matrices: gamma, ht, wt
-	//vectors: vals, b, cols, ind, pdfaltsumu/v
-	//sparse: Aa
+//M: constructMatrix
+void build_matrix (mat& A, vec& b, img2_dx,mat img2_dy,mat img_z,
+		mat dxx,mat dxy,mat dyy,mat img_dxz,mat e_data,
+		mat E_smooth,mat u,mat v,double gamma){
+	//Question here, what if matrix u is not a square?
+	//how do we use size() to get height and width then.
+
+	height = size(u);
+	width = size(u);
+	//or do height = u.n_rows; //Question: does that work?
+
+	//way to test what size u produces
+	//mat u (5,9);
+	//cout << "size of u: " << size(u) << endl;
+	//if this size doesn't work, try doing this and test again
+	//height = u.n_rows;
+	//width = u.n_cols;
+
+	//next frame off the matrix E_smooth with zeroes (not sure if correct)
+	//please test
+	int E_smoothheight = E_smooth.n_rows;
+	int E_smoothwidth = E_smooth.n_cols;
+	//top and bottom row to zero
+	E_smooth.row(0).zeros;
+	E_smooth.row(E_smoothheight-1).zeros;
+	//left and right cols to zero
+	E_smooth.col(0).zeros;
+	E_smooth.col(E_smoothwidth-1).zeros;
+	//see if this frames it off with zeros
+	//main concern is if the .zeros() function can be used this way.
+	//cout << "E_smooth after zero frame: " << E_smooth << endl;
+
+
+
+	// M: tmp = repmat( 1 : 2 * ht * wt, 6, 1 ) ;
+	// M: ros = tmp(:);
+	int temp4repmat = 2*height*width*6;
+	int tempPop = 1;
+	vec rows = (temp4repmat); //column vector of size temp4repmat
+	for (int i = 0; i<temp4repmat ; i++){
+		rows(i) = tempPop;
+		if(i%6 == 0){
+			tempPop++;
+		}
+	}
+
+	//M:cols = rows
 
   return;
 }
