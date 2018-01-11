9a154498eac637c0613e936d280a8435548dbc98
jgawiebe <s27508@rmc.ca>
2018-01-11 08:36:15 -0500

    test 2 visual studio linux

--- test.cpp
+++ test.cpp
@@ -6,26 +6,26 @@ Rev1: Nov 2017
 
 #include <iostream>
 #include <armadillo>
-#include <cv.hpp>
+//#include <cv>
 
 using namespace std;
-//using namespace arma;
-using namespace cv;
+using namespace arma;
+//using namespace cv;
 //g++ test.cpp -o test -O2 -larmadillo `pkg-config --cflags --libs opencv`; ./test
 
 //compile with: g++ test.cpp -o test -O2 -larmadillo; ./test
 int main(){
 
   //TESTING ARMA COMMANDS
-  // mat A(54, 36, fill::randu);
-  //
-  // SizeMat my_size = size(A);
-  // cout << "Size is: " << my_size << endl;
-  //
-  // int height = A.n_rows;
-	// int width = A.n_cols;
-  //
-  // cout << "Size is: " << height << 'x' << width << endl;
+  mat A(54, 36, fill::randu);
+  
+  SizeMat my_size = size(A);
+  cout << "Size is: " << my_size << endl;
+  
+  int height = A.n_rows;
+	int width = A.n_cols;
+  
+  cout << "Size is: " << height << 'x' << width << endl;
 
 
 //TESTING MATRIX_BUILDER
