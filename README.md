1e6cbc90941337fafa2120c4f2e9a441c1ea1cca
s27508 <s27508@rmc.ca>
2018-01-18 09:52:57 -0500

    Energy calc edit; testing eclipse

--- README.md
+++
@@ -1,2 +0,0 @@
-EEE457 4th Year Project: Parallelization of the Pixel Shiftmap Estimation Algorithm on a GPU
-By Jacob Wiebe and James Dolman
--- energy_calc.hpp
+++ energy_calc.hpp
@@ -23,14 +23,13 @@ T psi_function(T x)
 }
 
 //M: computePsidashFS_brox
-template <class T>
-mat generate_esmooth(T u, T v)
+mat generate_esmooth(mat u, mat v)
 {
   double height = u.n_rows;
   double width = u.n_cols;
   mat e_smooth(2 * height + 1, 2 * width + 1, fill::zeros);
   mat temp(1, 2);
-  mat temp.t(1, 2);
+  mat tr_temp(1, 2); //transposition of temp matrix
 
   temp(0) = 1;
   temp(1) = -1;
@@ -38,27 +37,27 @@ mat generate_esmooth(T u, T v)
   mat u_dx = conv2(u, temp);
   mat v_dx = conv2(v, temp);
 
-  mat u_dy = conv2(u, temp.t);
-  mat v_dy = conv2(v, temp.t);
+  mat u_dy = conv2(u, tr_temp);
+  mat v_dy = conv2(v, tr_temp);
 
   temp(1) = 1;
 
   mat u_dx2 = conv2(u_dx, temp / 2, "valid");
   mat v_dx2 = conv2(u_dx, temp / 2, "valid");
 
-  mat u_dy2 = conv2(u_dy, temp.t / 2, "valid");
-  mat v_dy2 = conv2(u_dy, temp.t / 2, "valid");
+  mat u_dy2 = conv2(u_dy, tr_temp / 2, "valid");
+  mat v_dy2 = conv2(u_dy, tr_temp / 2, "valid");
 
   mat delta_ux = conv2(u_dy2, temp / 2);       //t
   mat u_pdx = pow(u_dx, 2) + pow(delta_ux, 2); //uxpd
 
-  mat delta_uy = conv2(u_dy2, temp.t / 2);     //t
+  mat delta_uy = conv2(u_dy2, tr_temp / 2);     //t
   mat u_pdy = pow(u_dx, 2) + pow(delta_uy, 2); //uypd
 
   mat delta_vx = conv2(v_dy2, temp / 2);       //t
   mat v_pdx = pow(v_dx, 2) + pow(delta_vx, 2); //vxpd
 
-  mat delta_vy = conv2(v_dy2, temp.t / 2);     //t
+  mat delta_vy = conv2(v_dy2, tr_temp / 2);     //t
   mat v_pdy = pow(v_dx, 2) + pow(delta_vy, 2); //vypd
 
   for (int col_ix = 0; col_ix < e_smooth.n_cols; col_ix++)
